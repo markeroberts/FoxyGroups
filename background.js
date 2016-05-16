d5ab5b335f39a01b0b5c1effbc4b981d8557bbe7
Mark Roberts <m.e.roberts@live.com>
2016-05-16 11:21:12 +0200

    Existing code

---
+++ background.js
@@ -0,0 +1,150 @@
+var tabGroups = [],
+	activeTabGroup = null,
+	activeTabId = null,
+	extensionPageId = null,
+	activeListeners = true;
+
+// Create new tab group of open tabs when extension loaded
+chrome.tabs.query({}, function(tabs) {
+	var tabObjects = tabs.map(function(tab) {
+		return {
+			id: tab.id,
+			title: tab.title,
+			url: tab.url,
+			image: null
+		};
+	});
+
+	var tabGroup = new TabGroup();
+	tabGroup.active = true;
+	tabGroup.tabs = tabObjects;
+	tabGroups.push(tabGroup);
+	activeTabGroup = tabGroup.id;
+});
+
+// Extension button clicked
+chrome.browserAction.onClicked.addListener(function() {
+	// If extension page is already open
+	if(extensionPageId) {
+		chrome.tabs.get(extensionPageId, function(tab) {
+			// Close extension page if also active
+			if(tab.active) {
+				chrome.tabs.remove(extensionPageId);
+			}
+			// Otherwise refresh current tab image and switch to page
+			else {
+				captureActiveTab(function() {
+					chrome.tabs.update(extensionPageId, {active: true});
+				});
+			}
+		});
+	}
+	// Otherwise refresh current tab image and open extension page
+	else {
+		activeListeners = false;
+		captureActiveTab(function() {
+			chrome.tabs.create({'url': chrome.extension.getURL('foxygroups.html')}, function(tab) {
+				extensionPageId = tab.id;
+				activeListeners = true;
+			});
+		});
+	}
+
+});
+
+// Get tab image when tab is made active or finished loading
+chrome.tabs.onActivated.addListener(function(activeInfo) {
+	if(activeListeners) captureActiveTab();
+});
+chrome.webNavigation.onCompleted.addListener(function(details) {
+	if(activeListeners) captureActiveTab();
+});
+
+// Remove tab image when tab is closed
+chrome.tabs.onRemoved.addListener(function(tabId) {
+	if(tabId === extensionPageId) {
+		extensionPageId = null;
+	}
+	console.log(extensionPageId);
+	// delete tabImages[tabId];
+});
+
+function TabGroup() {
+	this.id = (new Date()).getTime();
+	this.name = '';
+	this.active = false;
+	this.position = {
+		top: 10,
+		left: 10
+	};
+	this.size = {
+		width: 200,
+		height: 200
+	};
+	this.tabs = [{
+		id: null,
+		title: 'New Tab',
+		url:'chrome://newtab/',
+		image: null
+	}];
+}
+
+// function createTabGroup(tabs) {
+// 	var tabGroup = new TabGroup(tabs);
+// 	tabGroups.push(tabGroup);
+// 	return tabGroup;
+// }
+
+function getTabGroup(id) {
+	return tabGroups.find(function(tabGroup) {
+		return tabGroup.id === parseInt(id);
+	});
+}
+
+function openTabGroup(tabGroupId) {
+	// Close all tabs except Tab Groups
+	// Get list of tabIds in current group to close
+	var tabIds = getTabGroup(activeTabGroup).tabs.map(function(tab) {
+		return tab.id;
+	});
+
+	chrome.tabs.remove(tabIds, function() {
+		// Now open each tab in new tab group
+		console.log(getTabGroup(tabGroupId).tabs);
+		getTabGroup(tabGroupId).tabs.forEach(function(tab) {
+			console.log('CREATE: ' + tab.url);
+			chrome.tabs.create({url: tab.url});
+		});
+		activeTabGroup = tabGroupId;
+	});
+}
+
+// Capture image of tab by given id and save to active tab group
+function captureActiveTab(callback) {
+	chrome.tabs.query({active: true}, function(tabs) {
+		activeTabId = tabs[0].id;
+
+		// Check tab is still active
+		chrome.tabs.get(activeTabId, function(tab) {
+			if(tab.active) {
+				chrome.tabs.captureVisibleTab(null, {format: 'jpeg', 'quality': 1}, function(dataUrl) {
+					// Randomly fails to capture tab, try again
+					if(chrome.runtime.lastError || !dataUrl){
+						setTimeout(function() {
+							captureActiveTab(activeTabId);
+						}, 100);
+					}
+					else {
+						// Find tab to save image to
+						var tab = getTabGroup(activeTabGroup).tabs.find(function(tab) {
+							return tab.id === activeTabId;
+						});
+						tab.image = dataUrl;
+
+						if(callback) callback();
+					}
+				});
+			}
+		});
+	});
+}
---
+++ js/main.js
@@ -0,0 +1,142 @@
+var background = chrome.extension.getBackgroundPage(),
+	tabImages = background.tabImages,
+	$tabgroups = $('#tabgroups');
+
+// Build each tab group on page load
+$(document).ready(function() {
+	// console.log(background.tabGroups);
+
+	background.tabGroups.forEach(function(tabGroup) {
+		buildTabGroup(tabGroup);
+	});
+});
+
+$tabgroups.dblclick(function(e) {
+	// Calculate new tab group position to avoid overlap with page boundary
+	var top = e.pageY,
+		left = e.pageX,
+		windowWidth = $(window).width(),
+		windowHeight = $(window).height();
+	if(top < 10) top = 10;
+	else if(top + 210 > windowHeight) top = windowHeight - 210;
+	if(left < 10) left = 10;
+	else if(left + 210 > windowWidth) left = windowWidth - 210;
+
+	var tabGroup = new background.TabGroup();
+	tabGroup.active = true;
+	tabGroup.position = {
+		top: top,
+		left: left
+	};
+	background.tabGroups.push(tabGroup);
+	activeTabGroup = tabGroup.id;
+
+	buildTabGroup(tabGroup);
+});
+
+$tabgroups.on('resize', '.tabgroup', function(e, ui) {
+	sizeTabsInGroup(ui.element);
+});
+// Save tabgroup position on dragstop
+$tabgroups.on('dragstop', '.tabgroup', function(e, ui) {
+	background.getTabGroup(ui.helper.attr('id')).position = ui.position;
+});
+// Save tabgroup size on resizestop
+$tabgroups.on('resizestop', '.tabgroup', function(e, ui) {
+	background.getTabGroup(ui.element.attr('id')).size = ui.size;
+});
+
+// Ensure tab sorting placeholder is same size as tabs to misalignment
+$tabgroups.on('sortstart', '.tabs', function(e, ui) {
+	ui.placeholder.width(ui.placeholder.siblings('.tab')[0].getBoundingClientRect().width);
+	ui.placeholder.height(ui.placeholder.siblings('.tab')[0].getBoundingClientRect().height);
+});
+
+// Switch to tab on click and close Tab Groups
+$tabgroups.on('click', '.tab', function() {
+	// Check if opening tab in current tab group
+	var tabGroupId = parseInt($(this).parents('.tabgroup').attr('id'));
+	if(tabGroupId === background.activeTabGroup) {
+		// chrome.tabs.update(parseInt($(this).attr('id')), {'active': true});
+		// chrome.tabs.getCurrent(function(tab) {
+		// 	chrome.tabs.remove(tab.id);
+		// });
+	}
+	else {
+		background.openTabGroup(tabGroupId);
+	}
+});
+
+// Creates a new tab group from given tabs at position x,y
+// function buildTabGroup(tabs, x, y) {
+function buildTabGroup(tabGroup) {
+	// Build each tab and add into .tabs div
+	var $tabs = $('<div>').addClass('tabs');
+
+	tabGroup.tabs.forEach(function(tab) {
+		$tabs.append($('<div>').addClass(tab.id === background.activeTabId ? 'tab active' : 'tab')
+				.attr('id', tab.id)
+				.append($('<div>').addClass('content')
+					.append($('<div>').addClass('image')
+						.append($('<img>').attr('src', tab.image))
+					)
+					.append($('<div>').addClass('title').text(tab.title))
+				)
+				.append($('<div>').addClass('tooltip')
+					.append($('<span>').text(tab.title))
+					.append($('<span>').text(tab.url))
+				)
+		);
+	});
+
+	// Build tab group
+	var group = $('<div>').addClass('tabgroup')
+		.attr('id', tabGroup.id)
+		.css('top', tabGroup.position.top).css('left', tabGroup.position.left)
+		.css('width', tabGroup.size.width).css('height', tabGroup.size.height)
+		.append('<div class="name">' + tabGroup.name + '</div>')
+		.append(
+			$tabs.sortable({
+				connectWith: '.tabs',
+				handle: '.content',
+				placeholder: 'tab',
+				containment: '#tabgroups'
+			})
+		)
+		.appendTo($tabgroups)
+		.draggable({
+			containment: '#tabgroups'
+		})
+		.resizable({
+			handles: 'se'
+		});
+
+	sizeTabsInGroup(group);
+}
+
+function sizeTabsInGroup($group) {
+	var $tabs = $group.find('.tabs'),
+		$tab  = $group.find('.tab'),
+		tabMinSize = 100;
+
+	var tabsBottom = $tabs.offset().top + $tabs.outerHeight(),
+		groupBottom = $group.offset().top + $group.outerHeight();
+
+	// Calculate how many tabs should fit in row
+	var tabsAcross = Math.floor($tabs.width() / tabMinSize);
+
+	// Check if this number allows vertical fit
+	var fitsVertically = false;
+	while(!fitsVertically) {
+		var numberOfRows = Math.ceil($tab.length / tabsAcross),
+			tabsHeight   = numberOfRows * ($tabs.width()/tabsAcross) * 0.8;
+		if($tabs.offset().top + tabsHeight + 10 <= groupBottom) {
+			fitsVertically = true;
+		}
+		else {
+			tabsAcross += 1;
+		}
+	}
+
+	$tab.css('width', 100/tabsAcross + '%');
+}
